ab3e7a4d11c73700b992f2973785c5971996f92b
92796110+RyanBriggsDev@users.noreply.github.com <92796110+RyanBriggsDev@users.noreply.github.com>
2023-01-16 15:48:45 +0000

    layout + global css

--- src/components/global/Layout.jsx
+++ src/components/global/Layout.jsx
@@ -3,10 +3,19 @@ import Footer from '../../components/global/footer/Footer'
 
 export default function Layout ({ children }) {
     return (
-        <>
+        <div style={styles.layout}>
             <Nav />
             {children}
             <Footer />
-        </>
+        </div>
     )
+}
+
+const styles = {
+    layout: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        minHeight: '100vh',
+    }
 }
