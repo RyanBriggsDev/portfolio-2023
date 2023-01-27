26704f34dd02a8434de213bc0c23ceb1431719a5
92796110+RyanBriggsDev@users.noreply.github.com <92796110+RyanBriggsDev@users.noreply.github.com>
2023-01-27 11:33:29 +0000

    card  + about section

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,7 @@ import Layout from '../components/global/Layout'
 
 import '../styles/globals.css'
 import '../styles/nav.css'
+import '../styles/card.css'
 
 export default function App({ Component, pageProps }) {
   return (
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,10 @@ import Head from 'next/head'
 import Container from '../components/global/Container'
 import FullPage from '../components/global/FullPage'
 import Header from '../components/global/Header'
+import Card from '../components/global/Card'
+import Image from 'next/image'
+
+import logo from '../assets/images/logo.png'
 
 export default function Home() {
   return (
@@ -33,9 +37,43 @@ export default function Home() {
           <Header 
             h2={`About Me`}
             h2ClassName='font-3'
-            label={`Learn a bit, or a lot, about me.`}
+            label={`Learn a bit about me.`}
             labelClassName='font-2'
           />
+          <div className='grid grid-2_60-40 gap-2'>
+            <Card 
+              cardBorder={true}
+              cardPadding={true}
+              cardRounded={true }
+              cardBg={true}
+              cardBgHover={true}
+            >
+              <div className='about-me flex f-d-col gap-1'>
+                <p>
+                  I am a self taught front end web developer with 1 year of experience in creating dynamic and responsive web applications using React and Next.js frameworks. 
+                </p>
+                <p>
+                  My expertise in HTML, CSS, and JavaScript allows me to build visually appealing and user-friendly websites. I have a solid understanding of the principles of component-based development, and am passionate about creating high-performance and scalable web applications that provide an exceptional user experience.
+                </p>
+                <p>
+                  I am a fast learner, and I am always eager to expand my skillset and improve my development process. I am excited to continue learning and growing as a developer, and am eager to apply my skills to real-world projects.
+                </p>
+                <p>
+                  In my free time, I love going for long walks with my dog, Ivy. I am also currently learning CAD modelling so I can 3D print my own designs.  
+                </p>
+              </div>
+            </Card>
+            <div id='hide-mobile' className='w-100 flex justify-center'>
+              <Image 
+                style={{objectFit: 'contain'}}
+                id='hide-mobile'
+                className='w-100' 
+                src={logo} 
+                alt='ryanbriggs.dev logo'
+                height={300}
+              />
+            </div>
+          </div>
         </Container>
       </FullPage>
 
@@ -47,6 +85,11 @@ export default function Home() {
             label={`What I've been working on.`}
             labelClassName='font-2'
           />
+          <div className="grid grid-3">
+            <Card width={'w-100'} cardBorder={true} cardPadding={true} cardRounded={true} cardBg={true}>
+              <Image src={logo} width={150} height={150}/>
+            </Card>
+          </div>
         </Container>
       </FullPage>
 
